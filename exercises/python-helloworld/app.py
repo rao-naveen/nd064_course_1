1b803779c1510b8e60a7e075c77297f51adcbfad
rao-naveen <rao.naveen@gmail.com>
2022-09-28 23:10:31 +0530

    Implemented helm charts

--- exercises/python-helloworld/app.py
+++ exercises/python-helloworld/app.py
@@ -1,9 +1,31 @@
+import json
+import logging
 from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
+    app.logger.info('Main request successful')
     return "Hello World!"
 
+@app.route('/status')
+def status():
+  response = app.response_class(
+          response=json.dumps({"result":"OK - healthy"}),
+          status=200,
+          mimetype='application/json'
+  )
+  return response
+
+@app.route('/metrics')
+def metrics():
+    response = app.response_class(
+        response = json.dumps({"test": "metrics"}),
+        status=200,
+        mimetype='application/json'
+    )
+    return response
+
 if __name__ == "__main__":
+    logging.basicConfig(filename='app.log',level=logging.DEBUG)
     app.run(host='0.0.0.0')
